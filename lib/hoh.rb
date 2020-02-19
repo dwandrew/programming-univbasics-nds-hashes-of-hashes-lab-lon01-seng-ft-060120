7cf8a2d621161d6d9ecf9d5fc6d03685adc25c91
Daniel Andrew <dwandrew@hotmail.com>
2020-02-19 11:25:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,22 +55,22 @@ PORTION_1 = {
      }
    }
  }
+} 
+ #PORTION_2 = {
+   #label: "Order"
+ #}
  
- PORTION_2 = {
-   label: "Order"
- }
+ #PORTION_3 = {
+   #label: "Family",
+   #sub_category: {
+   #  label: "Genus",
+  # }
+ #}
  
- PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
-   }
- }
- 
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
- }
+ #PORTION_4 = {
+   #label: "Species",
+  # sub_category: nil
+ #}
 
   # Remember:
   #  Kingdom
