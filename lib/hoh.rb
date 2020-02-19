d9af072a114bb2e7d9a369e1c3a8d025b0971249
Daniel Andrew <dwandrew@hotmail.com>
2020-02-19 11:24:39 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,7 +37,21 @@ PORTION_1 = {
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
+       sub_category: {
+         label: "Order",
+         sub_category : {
+           label: "Family",
+           sub_category: {
+             label: "Genus",
+             sub_category:{
+               label: "Species",
+               sub_category: nil
+            
+             }
+           }
+         
+       }
      }
    }
  }
